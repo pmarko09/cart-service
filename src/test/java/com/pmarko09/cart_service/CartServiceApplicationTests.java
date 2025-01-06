c7094220904b9e64ecc727f8f5f6da31823a2e18
pmarko <p.marko09@yahoo.com>
2025-01-06 13:42:26 +0100

    initial commit

---
+++ src/test/java/com/pmarko09/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmarko09.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
